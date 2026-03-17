bc55d463c3f78d23e857027b37545d45309fd641
joelBit <joel.rollny@gmail.com>
2026-03-17 12:06:45 +0100

    Created ReviewRepositoryTest

--- src/main/java/api/_bit/catalog/domain/Review.java
+++ src/main/java/api/_bit/catalog/domain/Review.java
@@ -1,8 +1,6 @@
 package api._bit.catalog.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
---
+++ src/main/java/api/_bit/catalog/repositories/ReviewRepository.java
@@ -0,0 +1,10 @@
+package api._bit.catalog.repositories;
+
+import api._bit.catalog.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByGameId(long id);
+}
