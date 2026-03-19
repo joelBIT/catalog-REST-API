41ba47c065aabe761f69aed58832e923cc6079a9
joelBit <joel.rollny@gmail.com>
2026-03-19 10:45:13 +0100

    Created DeveloperServiceTest

---
+++ src/test/java/api/_bit/catalog/services/DeveloperServiceTest.java
@@ -0,0 +1,44 @@
+package api._bit.catalog.services;
+
+import api._bit.catalog.repositories.GameRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+@ExtendWith(MockitoExtension.class)
+public class DeveloperServiceTest {
+
+    @Mock
+    private GameRepository gameRepository;
+
+    @InjectMocks
+    private DeveloperServiceImpl developerService;
+
+    @BeforeEach
+    void setUp() {
+        developerService = new DeveloperServiceImpl(gameRepository);
+    }
+
+    @Test
+    public void shouldReturnAllDistinctDevelopers() {
+        List<String> developers = new ArrayList<>();
+        developers.add("Capcom");
+        developers.add("Konami");
+        when(gameRepository.findDistinctDevelopers()).thenReturn(developers);
+
+        List<String> result = developerService.getDevelopers();
+
+        assertEquals(2, result.size());
+        verify(gameRepository, times(1)).findDistinctDevelopers();
+    }
+}
